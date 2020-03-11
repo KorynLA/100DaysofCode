5e1e415b38a56ec4361150ad9b373768ecd99fb2
KorynLA <leslieak@oregonstate.edu>
2020-03-10 18:59:24 -0700

    Starting finance tracker

---
+++ Finance_tracker/Bills.java
@@ -0,0 +1,11 @@
+public class Bills {
+	private int bills;
+	
+	Bills(int bills) {
+		this.bills = bills;
+	}
+	
+	public void view_bills() {
+		System.out.println(bills);
+	}
+}
---
+++ Finance_tracker/Finances.java
@@ -0,0 +1,13 @@
+import java.util.*;
+//import Bills;
+/***
+
+***/
+
+public class Finances {
+	public static void main(String[] args) {
+		System.out.println("View your finances");
+		Bills phone= new Bills(10);
+		phone.view_bills();
+	}
+}
